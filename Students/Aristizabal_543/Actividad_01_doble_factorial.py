65dbf91ddeff24ff6fe7db85ad4cb9697d4a869a
eduardo-aristizabal <159176238+eduardo-aristizabal@users.noreply.github.com>
2024-02-18 11:50:32 -0500

    Add files via upload

---
+++ Students/Aristizabal_543/Actividad_01_doble_factorial.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def doble_factorial(n):
+    if n<=0:
+        return 1
+    else:
+        return n* doble_factorial(n-2)
+    
+n = int(input("ingrese el número del cual se desea conocer su doble factorial: "))
+print("El doble factorial de ",n, "es: ", doble_factorial(n))
+
