b3ca945a0c79735f015641b5c1471d76ade8993d
alexphy08 <159055735+alexphy08@users.noreply.github.com>
2024-02-21 12:55:00 -0500

    Add files via upload

--- Students/Gómez_613/Actividades/Actividad01/actividad_01_lucas.py
+++ Students/Gómez_613/Actividades/Actividad01/actividad_01_lucas.py
@@ -1,10 +1,13 @@
+
 def lucas(n):
-    n=n-1
+    '''
+    Esta función devuelve el n-ésimo término de la serie de Lucas.
+    '''
     l=[2,1]
     for i in range(2,n+1):
         luc=l[i-1]+l[i-2]
         l.append(luc)
     return l[n]
 
-n=int(input('Ingrese la posición del número de Lucas que quiera: '))
-print(lucas(n))
+n=int(input('Ingrese la posición del número de Lucas que desea: '))
+print(f'El número de Lucas que ocupa la posición {n} es: {lucas(n)}')
